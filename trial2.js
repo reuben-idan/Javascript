0b36d55b47a58bbb70c994f04597806275533a82
reuben-idan <nobeadonis2@gmail.com>
2025-01-30 09:37:43 +0000

    Prompts

--- trial2.js
+++ trial2.js
@@ -1,6 +1,5 @@
-let height = Number(window.prompt("Enter the height of the box: "));
-let width = Number(window.prompt("Enter the width of the box: "));
-let depth = Number(window.prompt("Enter the depth of the box: "));
-let volume = height * width * depth;
-window.alert("The volume of the box is " + volume);
-
+let width = prompt("Volume of the box, enter width", 0);
+let height = prompt("Volume of the box, enter height", 0);
+let length = prompt("Volume of the box, enter length", 0);
+let volume = width * height * length;
+alert(`Calculated box volume is ${volume}`);
