85d9a78a10ca0fcd9cd283ec4bbee45b3a417dbc
reuben-idan <nobeadonis2@gmail.com>
2025-01-30 13:59:02 +0000

    Calculator

--- conditions.js
+++ conditions.js
@@ -1,12 +1,25 @@
-// Exercise 1: Write a script that will ask the user to input a number.
-// Display the message "Bingo!" when the number is greater than 90
-//  but less than 110, otherwise display the message: "Miss". Use the if statement.
+// Exercise 3: Write a simple calculator application. Ask the user for the following input, 
+// one by one: two numbers and a character representing a mathematical operation,
+//  one of "+", "-", "*", "/". If the user input is valid, calculate the result and 
+// show it to the user. If the user input is invalid, display a message that informs 
+// the user that an error has occurred.
 
-let number = Number(prompt("Please enter a number", 0));
-// if (number > 90 && number < 110) {
-//   alert("Bingo!");
-// } else {
-//   alert("Miss");
-// }
+// Remember that the value returned by the prompt function is of the type string. 
+// You can use the Number.isNaN method to check if you get the correct number after conversion. 
+// For example, calling Number.isNaN(10) will return false, while Number.isNaN(NaN) will return true.
 
-(number > 90 && number < 110) ? alert("Bingo!") : alert("Miss");
+// The program should work like this:
+Numb1 = Number(prompt("Enter the first number: "));
+Numb2 = Number(prompt("Enter the second number: "));
+Operation = prompt("Enter the operation: ");
+if (Operation == "+" && !Number.isNaN(Numb1) && !Number.isNaN(Numb2)) {
+    alert(Numb1 + Numb2);
+    } else if (Operation == "-" && !Number.isNaN(Numb1) && !Number.isNaN(Numb2)) {
+        alert(Numb1 - Numb2);
+    } else if (Operation == "*" && !Number.isNaN(Numb1) && !Number.isNaN(Numb2)) {
+        alert(Numb1 * Numb2);
+    } else if (Operation == "/" && !Number.isNaN(Numb1) && !Number.isNaN(Numb2)) {
+        alert(Numb1 / Numb2);
+    } else {
+        alert("Error");
+}
