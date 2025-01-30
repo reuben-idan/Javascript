cc0dc9517978e5bb9563516554e357ba7a25e7d0
reuben-idan <nobeadonis2@gmail.com>
2025-01-30 09:23:34 +0000

    Javascript Review

---
+++ trial.js
@@ -0,0 +1,14 @@
+contactName1 = "Maxwell Wright"; //Curabitur.egestas.nunc@nonummyac.co.uk
+contactName2 = "Raja Villarreal"; //posuere.vulputate@sed.com
+contactName3 = "Helen Richards"; //libero@convallis.edu
+
+contactPhone1 = "(0191) 719 6495";
+contactPhone2 = "0866 398 2895";
+contactPhone3 = "0800 1111";
+
+contactEmail1 = "Curabitur.egestas.nunc@nonummyac.co.uk";
+contactEmail2 = "posuere.vulputate@sed.com";
+contactEmail3 = "libero@convallis.edu";
+
+console.log(`${contactName1}/${contactPhone1}/${contactEmail1}`);
+console.log(`${contactName3}/${contactPhone3}/${contactEmail3}`);
---
+++ trial2.js
@@ -0,0 +1,6 @@
+let height = Number(window.prompt("Enter the height of the box: "));
+let width = Number(window.prompt("Enter the width of the box: "));
+let depth = Number(window.prompt("Enter the depth of the box: "));
+let volume = height * width * depth;
+window.alert("The volume of the box is " + volume);
+
