e7db06ac67d3a8bd711d9d8f69be1493ada072a1
reuben-idan <nobleadonis2@gmail>
2025-02-07 16:50:16 +0000

    Update function

--- functions.js
+++ functions.js
@@ -1,12 +1,7 @@
-const add = (a, b) =>
-  Number.isInteger(a) && Number.isInteger(b) ? a + b : NaN;
-const sub = (a, b) =>
-  Number.isInteger(a) && Number.isInteger(b) ? a - b : NaN;
-const mult = (a, b) =>
-  Number.isInteger(a) && Number.isInteger(b) ? a * b : NaN;
-add(1, 2); // 3
-sub(1, 2); // -1
-mult(1, 2); // 2
-add(1, "2"); // NaN
-
-console.log(sub(1, 2)); // -1
+let counter = 1;
+let intervalId = setInterval(function () {
+  console.log(counter++);
+}, 2000);
+setTimeout(function () {
+  clearInterval(intervalId);
+}, 20000);
